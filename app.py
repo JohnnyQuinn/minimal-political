af9bd7af7c8c1ab53e62330911736703e191354e
johnnyquinn <jftq97@gmail.com>
2021-01-12 10:08:42 -0500

    moved API call for list of members from get_lawmaker_info() to @app.route(/lawmaker-results), successfully made API for last 20 votes of a specific member and inserted that data into lawmaker-results.html

--- app.py
+++ app.py
@@ -11,10 +11,8 @@ pp = PrettyPrinter(indent=1)
 @app.route("/")
 def homepage():
 
-    full_congress_list = create_members_list(senate_members_data, house_members_data)
-
     context = {
-        'full_congress_list': full_congress_list,
+        'full_congress_list': "null",                   #do autocompleted search after getting everything to display on lawmaker-results.html
     }
 
     return render_template('home.html', **context)
@@ -36,8 +34,46 @@ def create_members_list(member_data1, member_data2):
 
     return member_list
 
-def get_lawmaker_info(name):
+def get_lawmaker_info(lawmaker_query, senate_members_data, house_members_data):
     """ takes in user inputted name of a lawmaker and returns the lawmaker's info (full name, chamber, id, party)"""
+
+    lawmaker_info = {
+        'name': '',
+        'id': '',
+        'chamber': '',
+        'party': ''
+    }
+    lawmaker_query = lawmaker_query.lower()
+    """
+    # TODO: update the condition to match the name search so that;
+        - it doesn't think its MacHenry when the query was henry 
+    """
+    # matches query based on name in the senate 
+    for member in senate_members_data:
+        lawmaker_full_name = f'{member["first_name"]} {member["last_name"]}'
+        if lawmaker_query in lawmaker_full_name.lower():
+            lawmaker_info['name'] = lawmaker_full_name
+            lawmaker_info['id'] = member['id']
+            lawmaker_info['chamber'] = 'Senate'
+            lawmaker_info['party'] = member['party']
+            break 
+    # if not in the senate then check the house
+        else: 
+            for member in house_members_data:
+                lawmaker_full_name = f'{member["first_name"]} {member["last_name"]}'
+                if lawmaker_query in lawmaker_full_name.lower():
+                    lawmaker_info['name'] = lawmaker_full_name
+                    lawmaker_info['id'] = member['id']
+                    lawmaker_info['chamber'] = 'House'
+                    lawmaker_info['party'] = member['party']
+                    break
+
+    return lawmaker_info
+
+@app.route("/lawmaker-results")
+def lawmaker_results():
+    # [ProPublica] GET list of members in senate and house 
+    # TODO: put API-key in .env and .gitignore
     senate_members_url = "https://api.propublica.org/congress/v1/116/senate/members.json"
     headers = {'x-api-key': "Hk6QVaUEQ453sdhadQMafiX9Ya5hblL7uwqVPEFw"}
 
@@ -51,47 +87,26 @@ def get_lawmaker_info(name):
 
     house_members_data = r["results"][0]["members"]
 
-    lawmaker_info = {
-        'name': '',
-        'id': '',
-        'chamber': '',
-        'party': ''
-    }
-    name = name.lower()
-
-    for member in house_members_data:
-        lawmaker_full_name = f'{member["first_name"]} {member["last_name"]}'
-        if name in lawmaker_full_name.lower():
-            lawmaker_info['name'] = lawmaker_full_name
-            lawmaker_info['id'] = member['id']
-            lawmaker_info['chamber'] = 'House'
-            lawmaker_info['party'] = member['party']
-
-    for member in senate_members_data:
-        lawmaker_full_name = f'{member["first_name"]} {member["last_name"]}'
-        if name in lawmaker_full_name.lower():
-            lawmaker_info['name'] = lawmaker_full_name
-            lawmaker_info['id'] = member['id']
-            lawmaker_info['chamber'] = 'Senate'
-            lawmaker_info['party'] = member['party']
-
-    return lawmaker_info
-
-@app.route("/lawmaker-results")
-def lawmaker_results():
+    # get user inputted query from searchbar
     lawmaker_query = request.args.get('lawmaker-query')
 
-    lawmaker_info = get_lawmaker_info(lawmaker_query)
+    # get their info (id, name, chamber, party)
+    lawmaker_info = get_lawmaker_info(lawmaker_query, senate_members_data, house_members_data)
 
-    request_URL = f'https://api.propublica.org/congress/v1/members/{lawmaker_info["id"]}/bills/introduced.json'
+    # [ProPublica] GET dictionary of the last 20 most recent bills voted on based on lawmakers's id 
+    # TODO: put API-key in .env and .gitignore
+    request_URL = f'https://api.propublica.org/congress/v1/members/{lawmaker_info["id"]}/votes.json'
     headers = {'x-api-key': "Hk6QVaUEQ453sdhadQMafiX9Ya5hblL7uwqVPEFw"}
 
     r = requests.get(request_URL, headers=headers).json()
 
+    recent_bills_voted = r['results'][0]['votes']
+
     context = {
         'lawmaker_name': lawmaker_info['name'],
         'lawmaker_chamber': lawmaker_info['chamber'],
-        'lawmaker_party': lawmaker_info['party']
+        'lawmaker_party': lawmaker_info['party'],
+        'recent_bills_voted': recent_bills_voted
     }
     return render_template('lawmaker-results.html', **context)
 
